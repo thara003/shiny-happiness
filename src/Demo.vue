26cfd7b66e0e7e6658ce014d6f1ed6454d88ec7f
Nayan Thara M <wittyhuminid@protonmail.com>
2022-10-11 14:35:59 +0530

    add numeric to the top bar

--- src/Demo.vue
+++ src/Demo.vue
@@ -7,48 +7,53 @@
         </div>
     </div>
     <a-tabs v-model:activeKey="activeKey" class="w-full text-black">
-        <!-- <a-tab-pane key="1"><template #tab>
-                <span class="text-slate-500">
-                    All
+        <a-tab-pane key="1"><template #tab>
+                <span class="text-slate-500 text-s" :class="[activeKey === '1' ? 'selected-tab' : '']">
+                    All 
                 </span>
+                <span class="ml-1 text-s font-semibold rounded px-2 py-0.5" :class="[activeKey === '1' ? 'bg-black text-white' : 'bg-slate-200 text-slate-400']">8</span>
             </template>
             Content of Tab Pane 1
         </a-tab-pane>
         <a-tab-pane key="2"><template #tab>
-                <span class="text-slate-500">
+                <span class="text-slate-500" :class="[activeKey === '2' ? 'selected-tab' : '']">
                     Following
                 </span>
+                <span class="ml-1 text-s font-semibold rounded px-2 py-0.5" :class="[activeKey === '2' ? 'bg-black text-white' : 'bg-slate-200 text-slate-400']">6</span>
             </template>
             Content of Tab Pane 2
         </a-tab-pane>
         <a-tab-pane key="3"><template #tab>
-                <span class="text-slate-500">
+                <span class="text-slate-500" :class="[activeKey === '3' ? 'selected-tab' : '']">
                     Archive
                 </span>
             </template>
             Content of Tab Pane 3
-        </a-tab-pane> -->
-        <a-tab-pane key="1" tab="All">Content of Tab 1</a-tab-pane>
-        <a-tab-pane key="2" tab="Following" force-render>Content of Tab Pane 2</a-tab-pane>
-        <a-tab-pane key="3" tab="Archive">Content of Tab Pane 3</a-tab-pane>
+        </a-tab-pane>
         <template #rightExtra>
             <setting-outlined :style="{fontSize: '20px'}" />
         </template>
     </a-tabs>
 </template>
-<style>
+<style >
 .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
-   color: #363636 !important; 
-   font-weight: 700;
-}
-:hover {
     color: #363636 !important;
+    font-weight: 700;
 }
+
+/* :hover {
+    color: #363636 !important;
+} */
 
 .ant-tabs-ink-bar {
     position: absolute;
     background: #000000 !important;
     pointer-events: none;
+}
+
+.selected-tab {
+    color: #000000 !important;
+    font-weight: 600;
 }
 </style>
 <script lang="ts">
